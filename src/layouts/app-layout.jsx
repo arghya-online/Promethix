8c64954080b79d387b21fcc3194e32f76e14322c
arghya-online <arghya0nline555@gmail.com>
2025-12-26 13:00:56 +0530

    refactor: simplify AppLayout by removing animation effects

--- src/layouts/app-layout.jsx
+++ src/layouts/app-layout.jsx
@@ -1,27 +1,15 @@
-import { Outlet, useLocation } from "react-router-dom";
-import { AnimatePresence, motion } from "framer-motion";
+import { Outlet } from "react-router-dom";
 import Header from "../components/Header.jsx";
 import { Footer } from "../components/Footer.jsx";
 
 function AppLayout() {
-  const location = useLocation();
+
 
   return (
     <div className="min-h-screen flex flex-col font-body">
       <Header />
       <main className="grow">
-        <AnimatePresence mode="wait">
-          <motion.div
-            key={location.pathname}
-            initial={{ opacity: 0, y: 10 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: -10 }}
-            transition={{ duration: 0.3, ease: "easeOut" }}
-            className="h-full"
-          >
-            <Outlet />
-          </motion.div>
-        </AnimatePresence>
+        <Outlet />
       </main>
       <Footer />
     </div>
